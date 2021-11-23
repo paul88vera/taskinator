de9db61287bc15ebbd108b39ad1a202893b7a89b
paul88vera <paul88vera@gmail.com>
2021-11-23 14:19:17 -0600

    event handler completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskhandler = function(){
+var createTaskhandler = function(event) {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task";
   tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskhandler);
-
+formEl.addEventListener("submit", createTaskhandler);
 
